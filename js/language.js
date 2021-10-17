a53caa77ab37954bc523c97dc2b573d89fec7534
Chihiro Saito <saito@credge@co.jp>
2021-10-17 18:10:34 +0900

    first

---
+++ js/language.js
@@ -0,0 +1,28 @@
+(function () {
+     $(function () {
+         setLang('ja');
+
+         $('#lang').on('change', function () {
+             var lang = $('#lang').val();
+             setLang(lang);
+         });
+     });
+
+     function setLang(lang) {
+         log('lang=' + lang);
+
+        i18next.use(i18nextXHRBackend).init({
+            backend: {
+                loadPath: 'language/{{lng}}/string.json'
+            },
+            debug: false,
+            defaultLng: 'ja',
+            fallbackLng: false,
+            lng: lang,
+        }, function (err, t) {
+            jqueryI18next.init(i18next, $);
+            $('[data-i18n]').localize();
+        });
+     }
+
+ })();
