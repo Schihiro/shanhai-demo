506bac0015120d0c54101157914494e2ae591adf
Chihiro Saito <saito@credge@co.jp>
2021-11-08 21:38:27 +0900

    言語引き継ぎ

--- js/language.js
+++ js/language.js
@@ -1,29 +1,35 @@
 (function () {
     $(function () {
-        setLang('ja');
+        if(localStorage.getItem('lang') === '') {
+            setLang('ja');
+        } else {
+            setLang(localStorage.getItem('lang')); // ここにif文で、strageがあればそれを使うなければデフォルトのjaにする
+        }
 
-        $('.lang').on('change', function () {
+        $(".lang").val(localStorage.getItem('lang'));
+
+        $('.lang').on('change', function () {　　　　// inputをlabelに変更するだけで良い
             var lang = $('.lang').val();
             setLang(lang);
         });
     });
 
-    function setLang(lang) {
-        console.log('lang=' + lang);
-        localStorage.setItem('lang', lang);
-
-        i18next.use(i18nextXHRBackend).init({
-            backend: {
-                loadPath: '../language/{{lng}}/string.json'
-            },
-            debug: false,
-            defaultLng: 'ja',
-            fallbackLng: false,
-            lng: lang,
-        }, function (err, t) {
-            jqueryI18next.init(i18next, $);
-            $('[data-i18n]').localize();
-        });
-    }
-
 })();
+
+function setLang(lang) {
+    console.log('lang=' + lang);
+    localStorage.setItem('lang', lang);
+
+    i18next.use(i18nextXHRBackend).init({
+        backend: {
+            loadPath: '../language/{{lng}}/string.json'
+        },
+        debug: false,
+        defaultLng: 'ja',
+        fallbackLng: false,
+        lng: lang,
+    }, function (err, t) {
+        jqueryI18next.init(i18next, $);
+        $('[data-i18n]').localize();
+    });
+}
